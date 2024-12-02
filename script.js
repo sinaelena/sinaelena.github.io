1677ba0080f972e3c5258c61daa6353903238ac3
sinaelena <sina.lang@fh-potsdam.de>
2024-12-02 12:37:18 +0100

    alte webseite

--- script.js
+++
@@ -1,9 +0,0 @@
-// Funktion zum Umschalten der "active"-Klasse
-function toggleMenu() {
-  const menu = document.querySelector(".menu");  // Menü-Container
-  const menuIcon = document.querySelector(".menu-icon");  // Hamburger-Menü-Icon
-  
-  // Umschalten der "active"-Klasse
-  menu.classList.toggle("active");
-  menuIcon.classList.toggle("active");
-}
